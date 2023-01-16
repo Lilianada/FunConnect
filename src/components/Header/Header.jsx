46335b012333c37e338c517f8cbf2268edf686b9
Lilianada <lilibest30.lb@gmail.com>
2023-01-16 16:14:55 -0500

    Header and packages

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/images/Funconnect-logo.svg";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+        <Link >
+            <img src={Logo} alt="FunConnect Logo" />
+        </Link>
+    </header>
+  );
+}
