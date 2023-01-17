d6677a45e7ef5755abd814c941bb3c6eac30f138
Lilianada <lilibest30.lb@gmail.com>
2023-01-17 07:37:38 -0500

    Header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,9 +6,55 @@ import "./Header.scss";
 export default function Header() {
   return (
     <header>
-        <Link to="/" >
-            <img src={Logo} alt="FunConnect Logo" />
+      <div className="desktopHeader">
+        <Link to="/">
+          <img src={Logo} alt="FunConnect Logo" />
         </Link>
+        <nav className="navbar">
+          <ul className="navList">
+            <li className="navItem">
+              <Link to="/blog" className="navLink">
+                Blog
+              </Link>
+            </li>
+            <li className="navItem">
+              <Link to="/about" className="navLink">
+                About Us
+              </Link>
+            </li>
+            <li className="navItem">
+              <Link to="/contact" className="navLink">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="mobileHeader">
+        <Link to="/">
+          <img src={Logo} alt="FunConnect Logo" />
+        </Link>
+        <nav className="navbar">
+          <ul className="navList">
+            <li className="navItem">
+              <Link to="/blog" className="navLink">
+                Blog
+              </Link>
+            </li>
+            <li className="navItem">
+              <Link to="/about" className="navLink">
+                About Us
+              </Link>
+            </li>
+            <li className="navItem">
+              <Link to="/contact" className="navLink">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
