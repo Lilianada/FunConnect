6b2f5639e096e29f19418ace3e69b3e506515037
Lilianada <lilibest30.lb@gmail.com>
2023-01-17 14:42:40 -0500

    Hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Hero.scss";
+
+export default function Hero() {
+  return (
+    <section className="heroSection">
+        <div className="hero__content">
+
+        </div>
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
 
 export default function Home() {
   return (
     <main className="mainContainer">
         <Header/>
+        <Hero/>
     </main>
   );
 }
