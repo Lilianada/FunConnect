08b568740dc47f09eaeb6a91a8c3cc237ddac757
Lilianada <lilibest30.lb@gmail.com>
2023-01-18 12:37:01 -0500

    Benefits

--- src/components/Benefits/Benefits.jsx
+++ src/components/Benefits/Benefits.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { BsStars } from "react-icons/bs";
 import "./Benefits.scss";
 
 export default function Benefits() {
   return (
     <section className="benefitSection">
-        <div className="benefitSection__container">
-
+      <div className="benefitSection__container">
+        <h5 className="benefitTitle">
+          <span className="primaryText">Funconnect </span> allows you to do what
+          you want.
+        </h5>
+        <div className="benefitFlex_container">
+          <div className="benefitFlex_item">
+            <BsStars fill="#000" stroke="#000" className="benefitFlex_item__icon"/>
+            <h6 className="benefitFlex_item__title">Easy Bookings</h6>
+            <p className="benefitFlex_item__description">
+            Share your experiences, organize fun events and connect with like-minds.
+            </p>
+          </div>
         </div>
+      </div>
     </section>
   );
 }
