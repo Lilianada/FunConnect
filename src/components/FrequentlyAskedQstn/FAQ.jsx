4c3e965e5513b4e1ca90ea3be39a10bf5e6fdc90
Lilianada <lilibest30.lb@gmail.com>
2023-01-22 18:24:34 -0500

    FAQ Section

--- src/components/FrequentlyAskedQstn/FAQ.jsx
+++ src/components/FrequentlyAskedQstn/FAQ.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsPlusLg } from "react-icons/bs";
+import Blob from "../../assets/images/Blob.png";
+import "./FAQ.scss";
 
 export default function FAQ() {
   return (
     <section className="faqSection">
-        
+        <div className="faqContent">
+            <div className="faqContent_head">
+                <h2 className="faqTitle">
+                   <span className="primaryText">Frequently </span> Asked Questions
+                </h2>
+                <img src={Blob} alt="Blob" className="blob" />
+            </div>
+            <div className="faqContent_body">
+                <div className="faqContent_item">
+                    <p className="faqContent_text">What is Funconnect?</p>
+                    <BsPlusLg size={24} fill="#202020"/>
+                </div>
+                <div className="faqContent_item">
+                    <p className="faqContent_text">How can I download the Funconnect app?</p>
+                    <BsPlusLg size={24} fill="#202020"/>
+                </div>
+                <div className="faqContent_item">
+                    <p className="faqContent_text">Will I pay to use the Funconnect app?</p>
+                    <BsPlusLg size={24} fill="#202020"/>
+                </div>
+                <div className="faqContent_item">
+                    <p className="faqContent_text">What type of events will be featured on Funconnect?</p>
+                    <BsPlusLg size={24} fill="#202020"/>
+                </div>
+                <div className="faqContent_item">
+                    <p className="faqContent_text">Can I book an event?</p>
+                    <BsPlusLg size={24} fill="#202020"/>
+                </div>
+            </div>
+        </div>
     </section>
   );
 }
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Blog from "../components/Blog/Blog";
 import Explore from "../components/Explore/Explore";
 import Features from "../components/Features/Features";
 import Footer from "../components/Footer/Footer";
+import FAQ from "../components/FrequentlyAskedQstn/FAQ";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 
@@ -17,6 +18,7 @@ export default function Home() {
         <Features />
         <Explore />
         <Blog />
+        <FAQ/>
       </main>
       <Footer />
     </>
