117d3dda4564922a3916e69c617fc543d75fca53
Lilianada <lilibest30.lb@gmail.com>
2023-01-24 08:20:13 -0500

    Discover section

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -2,5 +2,15 @@ import React from "react";
 import "./Discover.scss";
 
 export default function Discover() {
-  return <div>Discover</div>;
+  return (
+    <section className="discoverSection">
+      <div className="discoverContent">
+        <h2 className="discoverTitle">
+          Discover events
+          <br />
+          <span className="whiteText">for you and create for others</span>
+        </h2>
+      </div>
+    </section>
+  );
 }
