ee40aec4b6795b19c773dc519bdc3c32b6cba1ce
Lilianada <lilibest30.lb@gmail.com>
2023-01-25 10:46:18 -0500

    explore cards

--- src/components/Data.js
+++ src/components/Data.js
@@ -70,3 +70,79 @@ export const Accordion=[
         content: "Yes. You would be able to book public events or private events that you have been invited to attend.periences.",
     },
 ]
+
+
+export const ExploreCards = [
+    {
+        id: 1,
+        place: "Brass & Copper",
+        location: "Lekki Phase 1",
+        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+    },
+    {
+        id: 2,
+        place: "OCEAN 5 BY RIVIERA",
+        location: "Femi Pearce, VI",
+        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+    },
+    {
+        id: 3,
+        place: "The Garden",
+        location: "Ikoyi",  
+        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+    },
+    {
+        id: 4,
+        place: "Waffle Way",
+        location: "Maitama, Abuja",  
+        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+    },
+    {
+        id: 5,
+        place: "Crush Cafe",
+        location: "Gwarinpa Estate, Abuja", 
+        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" 
+    },
+    {
+        id: 6,
+        place: "Thought Pyramid",
+        location: "Wuse, Abuja",  
+        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+    },
+    {
+        id: 7,
+        place: "The Peacock",
+        location: "Lekki Phase 1",  
+        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+    },
+    {
+        id: 8,
+        place: "Brass & Copper",
+        location: "Lekki Phase 1",  
+        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+    },
+    {
+        id: 9,
+        place: "Brass & Copper",
+        location: "Lekki Phase 1",  
+        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+    },
+    {
+        id: 10,
+        place: "Brass & Copper",
+        location: "Lekki Phase 1",  
+        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+    },
+    {
+        id: 11,
+        place: "Brass & Copper",
+        location: "Lekki Phase 1",  
+        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+    },
+    {
+        id: 12,
+        place: "Brass & Copper",
+        location: "Lekki Phase 1",  
+        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+    },
+]
--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ExploreCards } from "../Data";
 import "./Explore.scss";
 
 export default function Explore() {
@@ -9,7 +10,19 @@ export default function Explore() {
             <p className="exploreSubtitle">Explore amazing places in your neighbourhood Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel nisl faucibus enim, mauris </p>
 
             <div className="exploreCard_wrap">
-
+              {
+                ExploreCards.map((card) => {
+                return(
+                  <div className="exploreCard" key={card.id}>
+                  <img src={card.image} alt="Location Pic" />
+                  <div className="exploreCard_content">
+                    <h3 className="exploreCard_title">{card.place}</h3>
+                    <p className="exploreCard_subtitle">{card.location}</p>
+                  </div>
+                </div>
+                )
+                })
+              }
             </div>
         </div>
     </section>
