8691d48cb3f3eb94c1e86780e5f9c954384481f5
Lilianada <lilibest30.lb@gmail.com>
2023-01-25 16:27:40 -0500

    commit

--- src/components/Data.js
+++ src/components/Data.js
@@ -77,72 +77,108 @@ export const ExploreCards = [
         id: 1,
         place: "Brass & Copper",
         location: "Lekki Phase 1",
-        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+        image: "https://images.unsplash.com/photo-1613946069412-38f7f1ff0b65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmVzdGF1cmFudHN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
     },
     {
         id: 2,
         place: "OCEAN 5 BY RIVIERA",
         location: "Femi Pearce, VI",
-        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+        image: "https://images.unsplash.com/photo-1590846406792-0adc7f938f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
     },
     {
         id: 3,
         place: "The Garden",
         location: "Ikoyi",  
-        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+        image: "https://images.unsplash.com/photo-1585518419759-7fe2e0fbf8a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
     },
     {
         id: 4,
         place: "Waffle Way",
         location: "Maitama, Abuja",  
-        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+        image: "https://images.unsplash.com/photo-1543373072-69f3d4788832?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODh8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
     },
     {
         id: 5,
         place: "Crush Cafe",
         location: "Gwarinpa Estate, Abuja", 
-        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" 
+        image: "https://images.unsplash.com/photo-1567667778211-b19f5a4e1efe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" 
     },
     {
         id: 6,
         place: "Thought Pyramid",
         location: "Wuse, Abuja",  
-        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+        image: "https://images.unsplash.com/photo-1607537669513-8472b5a8f85b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODN8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
     },
     {
         id: 7,
         place: "The Peacock",
         location: "Lekki Phase 1",  
-        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+        image: "https://images.unsplash.com/photo-1559304822-9eb2813c9844?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
     },
     {
         id: 8,
         place: "Brass & Copper",
         location: "Lekki Phase 1",  
-        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+        image: "https://images.unsplash.com/photo-1521917441209-e886f0404a7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
     },
     {
         id: 9,
         place: "Brass & Copper",
         location: "Lekki Phase 1",  
-        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+        image: "https://images.unsplash.com/photo-1581486902120-5880052aadf0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
     },
     {
         id: 10,
         place: "Brass & Copper",
         location: "Lekki Phase 1",  
-        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+        image: "https://images.unsplash.com/photo-1550966871-3ed3cdb5ed0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjJ8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
     },
     {
         id: 11,
         place: "Brass & Copper",
         location: "Lekki Phase 1",  
-        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+        image: "https://images.unsplash.com/photo-1555992336-03a23c7b20ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
     },
     {
         id: 12,
         place: "Brass & Copper",
         location: "Lekki Phase 1",  
-        image: "https://images.unsplash.com/photo-1556740734-7c7a7f7b3f5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFycnMlMjBhbmQlMjBjb3BwZXIlMjBjb2xvciUyMGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+        image: "https://images.unsplash.com/photo-1573668200361-62e141908294?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nzh8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+    },
+    {
+        id: 13,
+        place: "Brass & Copper",
+        location: "Lekki Phase 1",  
+        image: "https://images.unsplash.com/photo-1579648207097-edbc62b94930?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTR8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+    },
+    {
+        id: 14,
+        place: "Brass & Copper",
+        location: "Lekki Phase 1",  
+        image: "https://images.unsplash.com/photo-1421622548261-c45bfe178854?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTN8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+    },
+    {
+        id: 15,
+        place: "Brass & Copper",
+        location: "Lekki Phase 1",  
+        image: "https://images.unsplash.com/photo-1560187839-af980ee37cec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTl8fHJlc3RhdXJhbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+    },
+    {
+        id: 16,
+        place: "Brass & Copper",
+        location: "Lekki Phase 1",  
+        image: "https://images.unsplash.com/photo-1508213824875-83a3d36e72a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA0fHxyZXN0YXVyYW50c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+    },
+    {
+        id: 17,
+        place: "Brass & Copper",
+        location: "Lekki Phase 1",  
+        image: "https://images.unsplash.com/photo-1591261730799-ee4e6c2d16d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTEzfHxyZXN0YXVyYW50c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+    },
+    {
+        id: 18,
+        place: "Brass & Copper",
+        location: "Lekki Phase 1",  
+        image: "https://images.unsplash.com/photo-1545069986-6684b1203ca5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE2fHxyZXN0YXVyYW50c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
     },
 ]
--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -1,30 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { ExploreCards } from "../Data";
 import "./Explore.scss";
 
 export default function Explore() {
+  const [hover, setHover] = useState(false);
+  const [hoverId, setHoverId] = useState(ExploreCards[0].id);
+
+  const isHover = () => {
+
+    setHover(!hover);
+  };
+
   return (
     <section className="exploreSection">
-        <div className="exploreContent">
-            <h2 className="exploreTitle">Explore Exciting Destinations</h2>
-            <p className="exploreSubtitle">Explore amazing places in your neighbourhood Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel nisl faucibus enim, mauris </p>
+      <div className="exploreContent">
+        <h2 className="exploreTitle">Explore Exciting Destinations</h2>
+        <p className="exploreSubtitle">
+          Explore amazing places in your neighbourhood Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Vel nisl faucibus enim, mauris{" "}
+        </p>
 
-            <div className="exploreCard_wrap">
-              {
-                ExploreCards.map((card) => {
-                return(
-                  <div className="exploreCard" key={card.id}>
-                  <img src={card.image} alt="Location Pic" />
+        <div className="exploreCard_wrap">
+          {ExploreCards.map((card) => {
+            return (
+              <div key={card.id}>
+                <div className="exploreCard"  onClick={isHover} >
+                  <div className="exploreImage">
+                    <img src={card.image} alt="Location Pic" />
+                  </div>
                   <div className="exploreCard_content">
-                    <h3 className="exploreCard_title">{card.place}</h3>
+                    <h6 className="exploreCard_title">{card.place}</h6>
                     <p className="exploreCard_subtitle">{card.location}</p>
                   </div>
                 </div>
-                )
-                })
-              }
-            </div>
+                {
+                  hover && (
+                <span className="hoverContent">
+                  <div className="exploreImage">
+                    <img src={card.image} alt="Location Pic" />
+                  </div>
+                  <div className="exploreCard_content">
+                    <h6 className="exploreCard_title">{card.place}</h6>
+                    <p className="exploreCard_subtitle">{card.location}</p>
+                  </div>
+                </span>
+                  )
+                }
+              </div>
+            );
+          })}
         </div>
+      </div>
     </section>
   );
 }
