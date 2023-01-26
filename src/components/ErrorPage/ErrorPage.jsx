cb10cb2cd62c9cfae1ab3d83b27e9ca4d0badd44
Lilianada <lilibest30.lb@gmail.com>
2023-01-26 16:41:49 -0500

    404 Page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -5,13 +5,15 @@ import './ErrorPage.scss';
 export default function ErrorPage() {
   return (
     <section className='errorPage'>
-      <img src={Logo} alt="Logo"/>
-        <h3 className='errorPage__subtitle'>404</h3>
-        <h1 className='errorPage__title'>You’re officially in no man’s land.</h1>
-        <p className='errorPage__text'>Ooops! Check the URL and try again.</p>
-        <button className="errorPage__button">
-          Go back home
-        </button>
+      <div className="errorContent">
+        <img src={Logo} alt="Logo"/>
+          <h3 className='errorPage__subtitle'>404</h3>
+          <h1 className='errorPage__title'>You’re officially in no man’s land.</h1>
+          <p className='errorPage__text'>Ooops! Check the URL and try again.</p>
+          <button className="errorPage__button">
+            Go back home
+          </button>
+      </div>
     </section>
   )
 }
