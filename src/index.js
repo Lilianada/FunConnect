9f0ffdf913905fcfc2226f9b3616ee1b356cbbc2
Lilianada <lilibest30.lb@gmail.com>
2023-01-26 11:20:16 -0500

    autoprefixer

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import Error from './pages/Error';
 import './index.css';
+import AboutUs from './pages/AboutUs';
+import Contact from './pages/Contact';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
-  // {
-  //   path: "*",
-  //   element: <App />,
-  //   errorElement: <ErrorPage />,
-  // },
+  {
+    path: "/about",
+    element: <AboutUs />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "*",
+    errorElement: <Error />,
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
