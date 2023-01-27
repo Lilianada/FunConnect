b3c3fe95f6aeabdf1a3142dc26b774ae1590ecad
Lilianada <lilibest30.lb@gmail.com>
2023-01-27 15:54:58 -0500

    contact form

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { HiArrowUpRight } from "react-icons/hi";
+import "./style.css";
 
 export default function ContactForm() {
-  return <div>ContactForm</div>;
+  return (
+   <section className="contactForm_wrap">
+      <div className="contactForm">
+        <h4 className="formHead">Send us a Message</h4>
+        <p className="formSubhead">Join thousands getting emails in their inbox.</p>
+
+        <form action="" method="post" className="contactForm" >
+          <input type="text" name="first-name" className="formField" placeholder="Enter first name" />
+          <input type="text" name="last-name" className="formField" placeholder="Enter last name" />
+          <input type="text" name="email-address" className="formField" placeholder="Enter your email address" />
+          <input type="text" name="phone-number" className="formField" placeholder="Enter your phone number" />
+          <textarea  name="text-area" className="formField" placeholder="Add message" cols="30" rows="10">
+          </textarea>
+          <button type="submit" className="formBtn">Send Message
+            <HiArrowUpRight className="formBtnIcon" />
+          </button>
+        </form>
+        <p className="formInform">
+        Funconnect uses contact information you provide us to contact you about our relevant content, product and services. You may unsubscribe from our list at anytime. For further information, read our Privacy Policy
+        </p>
+    </div>
+   </section>
+    );
 }
--- src/components/Footer/SubscriptionForm.tsx
+++ src/components/Footer/SubscriptionForm.tsx
@@ -3,67 +3,67 @@ import { HiArrowUpRight } from "react-icons/hi2";
 import "./style.scss";
 
 const SubscribeForm = () => {
-    const [status, setStatus] = useState<string | null>(null)
-    const [email, setEmail] = useState("")
-  
-    const FORM_URL = `https://app.convertkit.com/forms/4786902/subscriptions`
-  
-    const handleSubmit = async (event: React.FormEvent) => {
-      event.preventDefault()
-  
-      const data = new FormData(event.target as HTMLFormElement)
-  
-      try {
-        const response = await fetch(FORM_URL, {
-          method: "post",
-          body: data,
-          headers: {
-            accept: "application/json",
-          },
-        })
-  
-        setEmail("")
-        const json = await response.json()
-  
-        if (json.status === "success") {
-          setStatus("SUCCESS")
-          return
-        }
-      } catch (err) {
-        setStatus("ERROR")
-        console.log(err)
+  const [status, setStatus] = useState<string | null>(null)
+  const [email, setEmail] = useState("")
+
+  const FORM_URL = `https://app.convertkit.com/forms/4786902/subscriptions`
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+
+    const data = new FormData(event.target as HTMLFormElement)
+
+    try {
+      const response = await fetch(FORM_URL, {
+        method: "post",
+        body: data,
+        headers: {
+          accept: "application/json",
+        },
+      })
+
+      setEmail("")
+      const json = await response.json()
+
+      if (json.status === "success") {
+        setStatus("SUCCESS")
+        return
       }
+    } catch (err) {
+      setStatus("ERROR")
+      console.log(err)
     }
-  
-    const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { value } = event.target
-      setEmail(value)
-    }
-    
-    return (
-      <div>
-        {status === "SUCCESS" && (
-            <>
-            <p>
-              Welcome aboard{email ? `, ${email}` : ""}{" "}
-              <span role="img" aria-label="Ship">
-                🚢
-              </span>
-            </p>
-            <p>Please check your inbox to confirm the subscription!</p>
-            </>
-        )}
-        {status === "ERROR" && (
-            <>
-            <p>Oops, something went wrong...</p>
-            <p>
-              Please,{" "}
-              <button className="btnPrimary" onClick={() => setStatus(null)}>try again.</button>
-            </p>
-            </>
-        )}
-        {status === null && (
-          <form className="footeFlex_form" onSubmit={handleSubmit}>
+  }
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    setEmail(value)
+  }
+
+  return (
+    <div>
+      {status === "SUCCESS" && (
+        <>
+          <p>
+            Welcome aboard{email ? `, ${email}` : ""}
+            <span role="img" aria-label="Ship">
+              🚢
+            </span>
+          </p>
+          <p>Please check your inbox to confirm the subscription!</p>
+        </>
+      )}
+      {status === "ERROR" && (
+        <>
+          <p>Oops, something went wrong...</p>
+          <p>
+            Please,{" "}
+            <button className="btnPrimary" onClick={() => setStatus(null)}>try again.</button>
+          </p>
+        </>
+      )}
+      {status === null && (
+        <form className="footeFlex_form" onSubmit={handleSubmit}>
           <input
             type="email"
             name="email"
@@ -78,10 +78,10 @@ const SubscribeForm = () => {
             <input type="submit" className="submitBtn" />
             <HiArrowUpRight />
           </div>
-          </form>
-        )}
-      </div>
-    )
-  }
+        </form>
+      )}
+    </div>
+  )
+}
 
-  export default SubscribeForm;
+export default SubscribeForm;
