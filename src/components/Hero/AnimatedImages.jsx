469315fae94592a09ce04a037741a6ac2027e881
Lilianada <lilibest30.lb@gmail.com>
2023-01-27 14:40:58 -0500

    Anmimated Images

--- src/components/Hero/AnimatedImages.jsx
+++ src/components/Hero/AnimatedImages.jsx
@@ -6,14 +6,14 @@ import PhoneFrame from "../../assets/images/heroImages/PhoneFrame.png";
 import Frame from "../../assets/images/heroImages/Frame.png";
 
 export default function AnimatedImages() {
-  return <div>
-    <div className="animatedImages_wrap">
-          <div className="animatedImages_content">
-            <img src={SmallStar} alt="Animated Image" className="animatedImage_one" />
-            <img src={BigStar} alt="Animated Image"className="animatedImage_two"  />
-            <img src={PhoneFrame} alt="Animated Image" className="animatedImage_three" />
-            <img src={Frame} alt="Animated Image" className="animatedImage_four"  />
-            </div>
-        </div>
-  </div>;
+  return (
+  <div className="animatedImages_wrap">
+    <div className="animatedImages_content">
+      <img src={SmallStar} alt="Animation" className="animatedImage_one" />
+      <img src={BigStar} alt="Animation" className="animatedImage_two" />
+      <img src={PhoneFrame} alt="Animation" className="animatedImage_three" />
+      <img src={Frame} alt="Animation" className="animatedImage_four" />
+    </div>
+  </div>
+  )
 }
--- src/hooks/useLocoScroll.jsx
+++ src/hooks/useLocoScroll.jsx
@@ -6,15 +6,17 @@ export default function useLocoScroll(start) {
     useEffect(() => {
         
         if (!start) return;
-        const scrollEl = document.querySelector('[mainContainer]');
-        const locoScroll = new LocomotiveScroll({
+        const scroll = document.querySelector('[mainContainer]');
+
+         const locoScroll = new LocomotiveScroll({
             el: document.querySelector('[data-scroll-container]'),
             smooth: true,
             multiplier: 1,
             class: 'is-reveal',
-            // smoothMobile: true,
-            // getDirection: true,
-            // lerp: 0.1,
+            smoothMobile: true,
+            getDirection: true,
+            lerp: 0.1,
         });
+
     }, [start]);
 }
