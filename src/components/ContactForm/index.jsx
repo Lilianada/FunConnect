6a9ce781ad334d6abf1709df82cf52414d7b8ad8
Lilianada <lilibest30.lb@gmail.com>
2023-01-29 17:03:37 -0500

    contact form

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,29 +1,62 @@
 import React from "react";
-import { HiArrowUpRight } from "react-icons/hi";
-import "./style.css";
+import { HiArrowUpRight } from "react-icons/hi2";
+import "./style.scss";
 
 export default function ContactForm() {
   return (
-   <section className="contactForm_wrap">
+    <section className="contactForm_wrap">
       <div className="contactForm">
-        <h4 className="formHead">Send us a Message</h4>
-        <p className="formSubhead">Join thousands getting emails in their inbox.</p>
+        <div className="contactForm_header">
+          <h4 className="formHead">Send us a Message</h4>
+          <p className="formSubhead">
+            Join thousands getting emails in their inbox.
+          </p>
+        </div>
 
-        <form action="" method="post" className="contactForm" >
-          <input type="text" name="first-name" className="formField" placeholder="Enter first name" />
-          <input type="text" name="last-name" className="formField" placeholder="Enter last name" />
-          <input type="text" name="email-address" className="formField" placeholder="Enter your email address" />
-          <input type="text" name="phone-number" className="formField" placeholder="Enter your phone number" />
-          <textarea  name="text-area" className="formField" placeholder="Add message" cols="30" rows="10">
-          </textarea>
-          <button type="submit" className="formBtn">Send Message
+        <form action="" method="post" className="contactForm_form">
+          <input
+            type="text"
+            name="first-name"
+            className="formField"
+            placeholder="Enter first name"
+          />
+          <input
+            type="text"
+            name="last-name"
+            className="formField"
+            placeholder="Enter last name"
+          />
+          <input
+            type="text"
+            name="email-address"
+            className="formField"
+            placeholder="Enter your email address"
+          />
+          <input
+            type="text"
+            name="phone-number"
+            className="formField"
+            placeholder="Enter your phone number"
+          />
+          <textarea
+            name="text-area"
+            className="formTextarea"
+            placeholder="Add message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button type="submit" className="formButton">
+            Send Message
             <HiArrowUpRight className="formBtnIcon" />
           </button>
         </form>
         <p className="formInform">
-        Funconnect uses contact information you provide us to contact you about our relevant content, product and services. You may unsubscribe from our list at anytime. For further information, read our Privacy Policy
+          Funconnect uses contact information you provide us to contact you
+          about our relevant content, product and services. You may unsubscribe
+          from our list at anytime. For further information, read our Privacy
+          Policy
         </p>
-    </div>
-   </section>
-    );
+      </div>
+    </section>
+  );
 }
