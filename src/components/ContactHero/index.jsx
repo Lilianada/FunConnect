9d2f586ca2c5269ad46f8e19c41472285cda6ed8
Lilianada <lilibest30.lb@gmail.com>
2023-01-30 04:50:28 -0500

    Contact form placement

--- src/components/ContactHero/index.jsx
+++ src/components/ContactHero/index.jsx
@@ -3,8 +3,15 @@ import './style.scss';
 
 export default function ContactHero () {
   return (
-    <div>
-      
-    </div>
+    <section className="contactHero_section">
+      <div className="contactHero_texts">
+        <h2 className="contactHero_title">
+          <span className="primaryText">Connect </span>
+          with Us and Let's Talk</h2>
+        <p className="contactHero_subtitle">
+        Need assistance? Fill out the form and our friendly team will  respond to you promptly within 24 hours
+        </p>
+      </div>
+    </section>  
   )
 }
