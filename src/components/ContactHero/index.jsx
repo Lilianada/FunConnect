fab4dae738f3a74194faa4a2592a2be4576a43f3
Lilianada <lilibest30.lb@gmail.com>
2023-01-30 07:37:53 -0500

    Contact hero

--- src/components/ContactHero/index.jsx
+++ src/components/ContactHero/index.jsx
@@ -9,7 +9,7 @@ export default function ContactHero () {
           <span className="primaryText">Connect </span>
           with Us and Let's Talk</h2>
         <p className="contactHero_subtitle">
-        Need assistance? Fill out the form and our friendly team will  respond to you promptly within 24 hours
+        Need assistance? Fill out the form and our friendly team will  respond to you promptly within 24 hours.g
         </p>
       </div>
     </section>  
