51534de1c28601af9db2325a100bbc1f82ad7ef2
Lilianada <lilibest30.lb@gmail.com>
2023-01-31 06:03:27 -0500

    Contact Information

---
+++ src/components/ContactInfo/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ContactInfo() {
+  return (
+    <section className="contactInfo_section">
+        <div className="contactInfo_wrap">
+        </div>   
+    </section>
+  );
+}
--- src/components/Faq/index.jsx
+++ src/components/Faq/index.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Blob from "../../assets/images/Blob.png";
 import { Accordion } from "../Data";
 import FAQAccordion from "./FaqAccordion";
 import "./style.scss";
 
 export default function FAQ() {
+    const [selectItem, setSelectItem] = useState(1);
+
+    const selected = (index) => {
+        setSelectItem(index);
+    }
+
 
   return (
     <section className="faqSection" data-scroll-section>
@@ -25,6 +31,8 @@ export default function FAQ() {
                                 key={item.id}
                                 title={item.title}
                                 description={item.content}
+                                open={selectItem === item.id}
+                                onClick={selected}
                             />
                         )
                     })
