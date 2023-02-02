8891a3eddb91e5af1957072d06a0aa4c4ce9fd44
Lilianada <lilibest30.lb@gmail.com>
2023-02-02 16:44:25 -0500

    Our Culture

--- src/components/OurCulture/index.jsx
+++ src/components/OurCulture/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import CEO from '../../assets/images/Ceo.png';
+import './style.scss';
 
 export default function OurCulture() {
   return (
-    <div>
-      
-    </div>
+    <section className="ourCulture_section">
+      <div className="ourCulture_content">
+        <div className="ourCulture_bodyText">
+          <h2 className="ourCulture_title">
+            <span className="primaryText">Our </span>
+            Culture
+          </h2>
+          <p className="ourCulture_text">
+             Embark on a journey of daring and excitement with us! Our company embraces boldness, embraces fun and embraces the audacious in all we do. From heart-pumping adventures to one-of-a-kind experiences, we're dedicated to delivering the ultimate thrills and memories you'll treasure forever.
+          </p>
+        </div>
+        <div className="ourCulture_bodyImage">
+            <img src={CEO} alt="CEO Profile" />
+        </div>
+      </div>
+    </section>
   )
 }
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AboutHero from '../components/AboutHero';
 import Header from '../components/Header';
+import OurCulture from '../components/OurCulture';
 import OurStory from '../components/OurStory';
 
 export default function AboutUs() {
@@ -9,6 +10,7 @@ export default function AboutUs() {
     <Header />
     <AboutHero/>
     <OurStory/>
+    <OurCulture/>
   </main>
   )
 }
