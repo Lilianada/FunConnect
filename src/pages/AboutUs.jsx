99472ca24f177d50ea29e6d8d0908ad1944e3a8b
Lilianada <lilibest30.lb@gmail.com>
2023-02-02 11:55:53 -0500

    our story to about section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutHero from '../components/AboutHero';
 import Header from '../components/Header';
+import OurStory from '../components/OurStory';
 
 export default function AboutUs() {
   return (
     <main className="mainWrapper">
     <Header />
     <AboutHero/>
+    <OurStory/>
   </main>
   )
 }
