05c326c87944cdd163d00f892c2b180f2364d228
Lilianada <lilibest30.lb@gmail.com>
2023-02-03 10:17:51 -0500

    ...team section

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaTwitter, FaLinkedinIn } from "react-icons/fa";
 import Img1 from "../../assets/images/teamImages/img1.jpg";
 import './style.scss';
 
@@ -20,20 +21,76 @@ export default function Team () {
             <div className="profileImage">
               <img src={Img1} alt="Profile" />
             </div>
-          </div>
-          <div className="teamSection_profile">
-            <div className="profileImage">
-              <img src={Img1} alt="Profile" />
+            <div className="teamProfile_data">
+              <div className="profileData">
+                <p className="profileName">Amujo Oluwasomidotun</p>
+                <p className="teamPosition">CEO</p>
+              </div>
+              <div className="profileLinks">
+                <a href="/" className="footerFlex_icon">
+                  <FaTwitter fill="#000" size={14} />
+                </a>
+                <a href="/" className="footerFlex_icon linkedinIcon">
+                  <FaLinkedinIn size={14} fill="#000" />
+                </a>
+              </div>
             </div>
           </div>
           <div className="teamSection_profile">
             <div className="profileImage">
               <img src={Img1} alt="Profile" />
             </div>
+            <div className="teamProfile_data">
+              <div className="profileData">
+                <p className="profileName">Amujo Oluwasomidotun</p>
+                <p className="teamPosition">CEO</p>
+              </div>
+              <div className="profileLinks">
+                <a href="/" className="footerFlex_icon">
+                  <FaTwitter fill="#000" size={14} />
+                </a>
+                <a href="/" className="footerFlex_icon linkedinIcon">
+                  <FaLinkedinIn size={14} fill="#000" />
+                </a>
+              </div>
+            </div>
           </div>
           <div className="teamSection_profile">
             <div className="profileImage">
               <img src={Img1} alt="Profile" />
+            </div>
+            <div className="teamProfile_data">
+              <div className="profileData">
+                <p className="profileName">Amujo Oluwasomidotun</p>
+                <p className="teamPosition">CEO</p>
+              </div>
+              <div className="profileLinks">
+                <a href="/" className="footerFlex_icon">
+                  <FaTwitter fill="#000" size={14} />
+                </a>
+                <a href="/" className="footerFlex_icon linkedinIcon">
+                  <FaLinkedinIn size={14} fill="#000" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="teamSection_profile">
+            <div className="profileImage">
+              <img src={Img1} alt="Profile" />
+            </div>
+            <div className="teamProfile_data">
+              <div className="profileData">
+                <p className="profileName">Amujo Oluwasomidotun</p>
+                <p className="teamPosition">CEO</p>
+              </div>
+              <div className="profileLinks">
+                <a href="/" className="footerFlex_icon">
+                  <FaTwitter fill="#000" size={14} />
+                </a>
+                <a href="/" className="footerFlex_icon linkedinIcon">
+                  <FaLinkedinIn size={14} fill="#000" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutHero from '../components/AboutHero';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import OurCulture from '../components/OurCulture';
 import OurStory from '../components/OurStory';
@@ -13,6 +14,7 @@ export default function AboutUs() {
     <OurStory/>
     <OurCulture/>
     <Team/>
+    <Footer />
   </main>
   )
 }
