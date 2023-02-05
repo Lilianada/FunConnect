24b622b5d731b08e3a655b0432791d538e7952c6
Lilianada <lilibest30.lb@gmail.com>
2023-02-05 16:23:45 -0500

    fixes to contact page

--- src/components/ContactHero/index.jsx
+++ src/components/ContactHero/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import People from '../../assets/images/contactImage/People.png';
 import './style.scss';
 
 export default function ContactHero () {
   return (
     <section className="contactHero_section">
+      <div className="contactHero_content">
       <div className="contactHero_texts">
         <h2 className="contactHero_title">
           <span className="primaryText">Connect </span>
@@ -11,6 +13,8 @@ export default function ContactHero () {
         <p className="contactHero_subtitle">
         Need assistance? Fill out the form and our friendly team will  respond to you promptly within 24 hours.
         </p>
+      </div>
+      <img src={People} alt="People" className="contactHero_imageDesktop" />
       </div>
     </section>  
   )
