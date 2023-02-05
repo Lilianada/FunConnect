241c90e3869b4d6678ab4ad6d6868578d7030159
Lilianada <lilibest30.lb@gmail.com>
2023-02-05 17:48:27 -0500

    contact hero

--- src/components/ContactHero/index.jsx
+++ src/components/ContactHero/index.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import People from '../../assets/images/contactImage/MobilePeople.png';
-import './style.scss';
+import React from "react";
+import People from "../../assets/images/contactImage/MobilePeople.png";
+import "./style.scss";
 
-export default function ContactHero () {
+export default function ContactHero() {
   return (
     <section className="contactHero_section">
       <div className="contactHero_content">
-      <img src={People} alt="People" className="contactHero_imageMobile" />
-      <div className="contactHero_texts">
-        <h2 className="contactHero_title">
-          <span className="primaryText">Connect </span>
-          with Us and Let's Talk</h2>
-        <p className="contactHero_subtitle">
-        Need assistance? Fill out the form and our friendly team will  respond to you promptly within 24 hours.
-        </p>
+        <div className="contactHero_texts">
+          <h2 className="contactHero_title">
+            <span className="primaryText">Connect </span>
+            with Us and Let's Talk
+          </h2>
+          <p className="contactHero_subtitle">
+            Need assistance? Fill out the form and our friendly team will
+            respond to you promptly within 24 hours.
+          </p>
+        </div>
+        <img src={People} alt="People" className="contactHero_imageMobile" />
       </div>
-      </div>
-    </section>  
-  )
+    </section>
+  );
 }
