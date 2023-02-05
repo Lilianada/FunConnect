025638e386df07e8109f855d2f747ed879e4d415
Lilianada <lilibest30.lb@gmail.com>
2023-02-05 17:06:04 -0500

    ...contact section fixes

--- src/components/ContactHero/index.jsx
+++ src/components/ContactHero/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import People from '../../assets/images/contactImage/People.png';
+import People from '../../assets/images/contactImage/MobilePeople.png';
 import './style.scss';
 
 export default function ContactHero () {
   return (
     <section className="contactHero_section">
       <div className="contactHero_content">
+      <img src={People} alt="People" className="contactHero_imageMobile" />
       <div className="contactHero_texts">
         <h2 className="contactHero_title">
           <span className="primaryText">Connect </span>
@@ -14,7 +15,6 @@ export default function ContactHero () {
         Need assistance? Fill out the form and our friendly team will  respond to you promptly within 24 hours.
         </p>
       </div>
-      <img src={People} alt="People" className="contactHero_imageMobile" />
       </div>
     </section>  
   )
