2954b09c806560dba355a7e19b9070c7bd6d3cd9
Lilianada <lilibest30.lb@gmail.com>
2023-02-06 15:59:05 -0500

    contact and about section fixes

--- src/components/AboutHero/index.jsx
+++ src/components/AboutHero/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import People from '../../assets/images/aboutImages/HeroBg-mobile.png';
 import './style.scss';
 
 export default function AboutHero() {
   return (
     <section className="aboutHero_section">
+        <div className="aboutHero_content">
         <div className="aboutHero_texts">
             <h2 className="aboutHero_title">
                 <span className="primaryText">Escape </span>
                 the ordinary, discover adventure!
             </h2>
         </div>
+        <img src={People} alt="People" className="aboutHero_imageMobile" />
+      </div>
     </section>
   )
 }
