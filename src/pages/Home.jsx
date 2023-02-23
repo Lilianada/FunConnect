fdf899eaf51076d2abeeab78783b570262e74567
Lilianada <lilibest30.lb@gmail.com>
2023-02-23 15:57:25 -0500

    Terms of Use and Privacy Policies pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { gsap, Power3 } from "gsap";
 import BackToTop from "../components/BackToTop";
 import Benefits from "../components/Benefits";
@@ -10,44 +10,23 @@ import Footer from "../components/Footer";
 import FAQ from "../components/Faq";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import Preloader from "../components/Preloader";
 
 export default function Home() {
-  // const [preloader, setPreloader] = useState(true);
-  // const [timer, setTimer] = useState(4);
-
   let tl = new gsap.timeline();
   let ease = Power3.easeOut;
   
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setTimer((timer) => timer - 1);
-  //   }, 1000);
-  //   if (timer === 0) {
-  //     clearInterval(interval);
-  //     setPreloader(false);
-  //   }
-  //   return () => clearInterval(interval);
-  // }, [timer]);
-
   return (
-    <>
-      {/* {preloader ? (
-        <Preloader/>
-      ) : ( */}
-        <main className="mainContainer" id="mainContainer">
-          <Header />
-          <Hero timeline={tl} ease={ease} />
-          <Benefits />
-          <Features />
-          <Explore />
-          <Blog />
-          <FAQ />
-          <Discover />
-          <Footer />
-          <BackToTop />
-        </main>
-      {/* )} */}
-    </>
+    <main className="mainContainer" id="mainContainer">
+      <Header />
+      <Hero timeline={tl} ease={ease} />
+      <Benefits />
+      <Features />
+      <Explore />
+      <Blog />
+      <FAQ />
+      <Discover />
+      <Footer />
+      <BackToTop />
+    </main>
   );
 }
