0d3f26186bc235a4aad9f946c05d94f3391ad4af
Lilianada <lilibest30.lb@gmail.com>
2023-02-24 16:07:24 -0500

    add terms and condition page

--- src/pages/Policies.jsx
+++ src/pages/Policies.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../components/Header'
 
 export default function Policies() {
   return (
     <main className="mainContainer" id="mainContainer">
-      
+        <Header />
+
     </main>
   )
 }
