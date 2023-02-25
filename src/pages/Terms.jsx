8968798479c14b19f61d0bb41c522fb1f1a54a8e
Lilianada <lilibest30.lb@gmail.com>
2023-02-25 15:48:17 -0500

    Add Footer to Terms page

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Header from '../components/Header'
-import TermsCondition from '../components/TermsCondition'
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import TermsCondition from '../components/TermsCondition';
 
 export default function Terms() {
   return (
     <main className="mainContainer" id="mainContainer">
         <Header/>
         <TermsCondition/>
+        <Footer/>
     </main>
   )
 }
