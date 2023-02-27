b4ab298cfb9cbaef3911a88b12fa8e9b0e1c5a45
Lilianada <lilibest30.lb@gmail.com>
2023-02-27 11:42:27 -0500

    Fixes

--- src/components/OurCulture/index.jsx
+++ src/components/OurCulture/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import CEO from '../../assets/images/teamImages/Amujo Oluwasomidotun.jpg';
-import Bg from '../../assets/images/Group419.png';
-import './style.scss';
+import React from "react";
+import CEO from "../../assets/images/teamImages/Amujo Oluwasomidotun.jpg";
+import Bg from "../../assets/images/Group419.png";
+import BgMobile from "../../assets/images/Group420.png";
+import "./style.scss";
 
 export default function OurCulture() {
   return (
@@ -13,19 +14,23 @@ export default function OurCulture() {
             Culture
           </h2>
           <p className="ourCulture_text">
-             Embark on a journey of daring and excitement with us! Our company embraces boldness, embraces fun and embraces the audacious in all we do. From heart-pumping adventures to one-of-a-kind experiences, we're dedicated to delivering the ultimate thrills and memories you'll treasure forever.
+            Embark on a journey of daring and excitement with us! Our company
+            embraces boldness, embraces fun and embraces the audacious in all we
+            do. From heart-pumping adventures to one-of-a-kind experiences,
+            we're dedicated to delivering the ultimate thrills and memories
+            you'll treasure forever.
           </p>
         </div>
         <div className="ourCulture_bodyImage">
           <figure>
-            <img src={CEO} alt="CEO Profile" className='imgDesktop' />
+            <img src={CEO} alt="CEO Profile" className="imgDesktop" />
             <figcaption>Oluwasomidotun Amujo </figcaption>
-            <img src={Bg} alt="CEO Profile" className='bg' />
-            <p>C.E.O  </p>
+            <p>C.E.O </p>
           </figure>
-            <img src={Bg} alt="CEO Profile" className='bg' />
+          <img src={BgMobile} alt="CEO Profile" className="bgMobile" />
+          <img src={Bg} alt="CEO Profile" className="bg" />
         </div>
       </div>
     </section>
-  )
+  );
 }
