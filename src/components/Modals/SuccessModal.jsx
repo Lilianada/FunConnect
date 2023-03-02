be7a00a7e154e7ae3e2b944a41f6864d1098ce27
Lilianada <lilibest30.lb@gmail.com>
2023-03-02 17:14:08 -0500

    SuccessModal

---
+++ src/components/Modals/SuccessModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Backdrop from '../Backdrop';
+import './style.scss';
+
+export default function SuccessModal() {
+  return (
+    <Backdrop>
+      <section className="successModal">
+
+      </section>
+    </Backdrop>
+  )
+}
