653a9aeb9b7f9ecb31a7a56d4d73a6bac7504020
Lilianada <lilibest30.lb@gmail.com>
2023-03-10 02:51:34 -0500

    Partner register section

--- src/components/PartnerRegister/index.jsx
+++ src/components/PartnerRegister/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './style.scss';
 
-export default function PartnerRegister() {
+export default function PartnerRegisteration() {
   return (
-    <div>
-      
-    </div>
+    <section className="partnerRegister">
+      <div className="prContainer">
+        <div className="prContainer_row">
+          <p>How it works</p>
+          <h2>
+            Register as a 
+            <span className="primaryText">
+              Partner 
+            </span>
+            on Funconnect
+          </h2>
+          <p>We partner with hospitality and tourist industry</p>
+          <Link to='/registration-form' className="btnPrimary">Get Started Today</Link>
+        </div>
+
+      </div>
+    </section>
   )
 }
